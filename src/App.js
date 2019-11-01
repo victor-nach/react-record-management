160ddd16e4028e70f3b5cbd34aa142d458ff7620
victor <viheanaco@gmail.com>
2019-11-01 17:00:49 +0100

    ft: setup initial page #2

--- src/App.js
+++ src/App.js
@@ -1,12 +1,23 @@
 import React from 'react';
 import './App.css';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-     
+    <div className='container'>
+      <h1> Record Management</h1>
+      <div className='row'>
+        
+        <div className='add-user col-md-6 col-sm-6'>
+          <h2> Add user</h2>
+        </div>
+
+        <div className='col-md-6 col-sm-6'>
+          <h2>View Users</h2>
+        </div>
+
+      </div>
     </div>
-  );
+  )
 }
 
 export default App;
